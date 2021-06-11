da3a8a5732cdacd708a49ce787c517ca0d6a8d6e
Kristian David Rogelstad <k@kristiandavid.com>
2021-06-11 17:12:55 -0400

    instafix: remove instagram temporarily

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
-import feather from 'feather-icons'
 import SEO from '../components/seo';
-import Img from "gatsby-image"
 import Layout from '../components/layout'
 
 import styles from "./index.module.scss";
@@ -13,7 +11,6 @@ class RootIndex extends React.Component {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const siteDesc = get(this, 'props.data.site.siteMetadata.description')
     const siteImg = get(this, 'props.data.site.siteMetadata.image')
-    const ig = get(this, 'props.data.instagram.edges')
     const home = get(this, 'props.data.home.edges[0].node')
     return (
       <Layout location={this.props.location}>
@@ -25,24 +22,6 @@ class RootIndex extends React.Component {
         />
         <div className={styles.pageContent}>
           <div className={styles.intro} dangerouslySetInnerHTML={{__html: home.content.childMarkdownRemark.html}} />
-          <div className={styles.insta}>
-            <div>
-              <div className={styles.instaIntro}>
-                <span dangerouslySetInnerHTML={{__html: feather.icons['chevrons-down'].toSvg({'height': 15})}} /> Some Instagrams!
-              </div>
-              <a className={styles.instaGrid} href="https://www.instagram.com/iamwoodbeard" target="_blank" rel="noreferrer" aria-label="Go to Woodbeard's Instagram page" >
-                {ig.map((obj, index) => {
-                  return (
-                    <Img
-                      fluid={[obj.node.localFile.childImageSharp.fluid]}
-                      alt="Instagram Image"
-                      className={styles.instaImg}
-                      key={obj.node.id} />
-                  )
-                })}
-              </a>
-            </div>
-          </div>
         </div>
       </Layout>
     )
@@ -74,32 +53,6 @@ export const pageQuery = graphql`
               html
             }
           }
-        }
-      }
-    }
-    instagram: allInstaNode(limit: 14, filter: {localFile: {id: {ne: null}}}, sort: {fields: timestamp, order: DESC}) {
-      edges {
-        node {
-          id
-          localFile {
-            childImageSharp {
-              fluid(maxWidth: 600, maxHeight: 600, quality: 90) {
-                ...GatsbyImageSharpFluid_withWebp
-              }
-            }
-          }
-          permalink
-          carouselImages {
-            preview
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 600, maxHeight: 600, quality: 90) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-          }
-          caption
         }
       }
     }
